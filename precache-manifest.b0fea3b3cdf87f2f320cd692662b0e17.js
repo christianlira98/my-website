917bdbf6e9bb5f5e210ae75080c7482fa8c0758f
Christian Lira <christianlira98@gmail.com>
2020-03-26 18:48:23 -0300

    Updates

---
+++ precache-manifest.b0fea3b3cdf87f2f320cd692662b0e17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be70acd903e3ee7c5df4eb17dba1c44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735b2be36c3f5da9fb4a",
+    "url": "/static/css/main.50f6b100.chunk.css"
+  },
+  {
+    "revision": "6d7feb969afba31ff0ab",
+    "url": "/static/js/2.7a179cbe.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.7a179cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735b2be36c3f5da9fb4a",
+    "url": "/static/js/main.c9b72141.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  },
+  {
+    "revision": "3cf248f601634f91b6eec6b77c57d856",
+    "url": "/static/media/LRxGqsU(1).3cf248f6.jpg"
+  },
+  {
+    "revision": "bd043c8ede0c435db6bc3048cdbf643e",
+    "url": "/static/media/LRxGqsU-2.bd043c8e.jpg"
+  },
+  {
+    "revision": "5b2a4f24eecb827cbfcdbd226d2cf45d",
+    "url": "/static/media/self.5b2a4f24.jpg"
+  }
+]);
