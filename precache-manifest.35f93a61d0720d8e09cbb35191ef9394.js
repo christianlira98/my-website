fdb1a86745f0f7d2d652c12121595973e357b3fd
Christian Lira <christianlira98@gmail.com>
2020-03-26 19:13:25 -0300

    Updates

---
+++ precache-manifest.35f93a61d0720d8e09cbb35191ef9394.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d945e8bdc1aefa7416d2af17a2460be",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "4a3c453c403dc9b12d2f",
+    "url": "/my-website/static/css/main.d7264c8c.chunk.css"
+  },
+  {
+    "revision": "881019bb694954898b58",
+    "url": "/my-website/static/js/2.112ff79e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/my-website/static/js/2.112ff79e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3c453c403dc9b12d2f",
+    "url": "/my-website/static/js/main.8b2b3924.chunk.js"
+  },
+  {
+    "revision": "79aea920306bfa5cbe45",
+    "url": "/my-website/static/js/runtime-main.56fdbba2.js"
+  },
+  {
+    "revision": "3cf248f601634f91b6eec6b77c57d856",
+    "url": "/my-website/static/media/LRxGqsU(1).3cf248f6.jpg"
+  },
+  {
+    "revision": "bd043c8ede0c435db6bc3048cdbf643e",
+    "url": "/my-website/static/media/LRxGqsU-2.bd043c8e.jpg"
+  },
+  {
+    "revision": "5b2a4f24eecb827cbfcdbd226d2cf45d",
+    "url": "/my-website/static/media/self.5b2a4f24.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49f0a0183f130a777e6ae56a2b47e6bb.js"
+  "/my-website/precache-manifest.35f93a61d0720d8e09cbb35191ef9394.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
